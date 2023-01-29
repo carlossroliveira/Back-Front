4c9caa314a9c6a24e59e949c12c7d24b796b0dac
carlos oliveira <carlos.sroliveira@hotmail.com>
2023-01-29 19:39:28 -0300

    Criação de novo componente

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarInterface {
+  progress: number;
+}
+
+export const ProgressBar = (props: ProgressBarInterface) => {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        className="h-3 rounded-xl bg-violet-600 transition-all"
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        style={{ width: `${props.progress}%` }}
+      />
+    </div>
+  );
+};
